66b4e0b0233bfd2700c907f38677eaadeee96b06
emanoelcampos <emanoel.cv@gmail.com>
2024-05-10 06:15:01 -0300

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tabnews-term-alert',
+    version='0.1.0',
+    url='https://github.com/emanoelcampos/tabnews-keyword-alert/',
+    author='Emanoel Campos',
+    author_email='emanoel.cv@gmail.com',
+    description='Automated system that fetches, filters, and sends email alerts about keywords in posts titles from Tabnews',
+    packages=find_packages(),
+    install_requires=[],
+)
+
