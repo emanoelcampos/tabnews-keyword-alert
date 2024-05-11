505693f48ac224c9f2d0d0a24be40fb86cc5e877
emanoelcampos <emanoel.cv@gmail.com>
2024-05-11 06:43:02 -0300

    Refactoring the bot's execution time.

--- src/scheduler.py
+++ src/scheduler.py
@@ -3,12 +3,8 @@ from bot import main
 
 schedule = BlockingScheduler()
 
-"""@schedule.scheduled_job('cron', day_of_week='mon,tue,wed,thu,fri,sat,sun', hour=8, timezone='America/Sao_Paulo')
-def scheduled_job():
-    main()"""
 
-
-@schedule.scheduled_job('interval', minutes=3)
+@schedule.scheduled_job('cron', day_of_week='mon,tue,wed,thu,fri,sat,sun', hour=8, timezone='America/Sao_Paulo')
 def scheduled_job():
     main()
 
