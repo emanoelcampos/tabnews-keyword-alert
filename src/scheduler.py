f51d658a47f7a7c1ead33f197c7e34f01203e400
emanoelcampos <emanoel.cv@gmail.com>
2024-05-11 06:19:46 -0300

    Add scheduler file

---
+++ src/scheduler.py
@@ -0,0 +1,16 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+from bot import main
+
+schedule = BlockingScheduler()
+
+"""@schedule.scheduled_job('cron', day_of_week='mon,tue,wed,thu,fri,sat,sun', hour=8, timezone='America/Sao_Paulo')
+def scheduled_job():
+    main()"""
+
+
+@schedule.scheduled_job('interval', minutes=3)
+def scheduled_job():
+    main()
+
+
+schedule.start()
